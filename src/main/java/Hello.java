1bcb1a515cb789400311e8b275a7220a6b01dd7a
ecas0043-SOUBHAGYA-JAGAPUR <ecas0043@ecas0043.exzatech.in>
2023-01-16 12:16:20 +0530

    second commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,20 @@
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello soubahgya");
+      boolean isMad = true;
+      if(isMad == false )
+          System.out.println("not mad");
+
+         double myFirstValue = 20.00d;
+        double mySirstValue = 80.00d;
+        double myValueTotal = (myFirstValue + mySirstValue) * 100.00d;
+           System.out.println("myValueTotal =" + myValueTotal);
+           double theRemainder = myValueTotal % 40.00d;
+           System.out.println("theRemainder =" + theRemainder);
+           boolean isNoRemainder = (theRemainder == 0)? true:false;
+           System.out.println("isNoRemainder =" + isNoRemainder);
+           if(!isNoRemainder){
+               System.out.println("got someremainder");
+           }
+    }
 }
